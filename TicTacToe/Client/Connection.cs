536998144b467c9a368968d4bbbe5a790a25a646
Vahraz M <dibe3333@hotmail.com>
2012-10-15 13:51:58 +0200

    Testing

--- TicTacToe/Client/Connection.cs
+++ TicTacToe/Client/Connection.cs
@@ -2,11 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using Shared;
 
 namespace Client
 {
     class Connection
     {
+        public bool ConnectToServer()
+        {
+            TcpChannel tcpChannel = new TcpChannel();
+            ChannelServices.RegisterChannel(tcpChannel, false);
+
+            Type requiredType = typeof (GameServerConnector);
+
+            GameServerConnector remoteObject =
+                (GameServerConnector) Activator.GetObject(requiredType, "tcp://localhost:9998/TicTacToe");
+
+            return remoteObject.NewStateSince(DateTime.Now);
+            
+        }
 
     }
 }
--- TicTacToe/Client/Form1.cs
+++ TicTacToe/Client/Form1.cs
@@ -15,5 +15,21 @@ namespace Client
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Connection connect = new Connection();
+            label1.Text = connect.ConnectToServer().ToString();
+        }
     }
 }
