57821a69bf28ac39e1477bad85acccd63e3f7cc7
Calle Lindholm <lindholm.calle@gmail.com>
2012-10-15 11:57:51 +0200

    Try try again

--- TicTacToe/Server/Program.cs
+++ TicTacToe/Server/Program.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
 using System.Text;
+using Shared;
 
 namespace Server
 {
@@ -9,6 +13,27 @@ namespace Server
     {
         static void Main(string[] args)
         {
+            const int portNumber = 9998;
+            Console.WriteLine("TicTacToe Server started on port {0}", portNumber);
+
+            var tcpChannel = new TcpChannel(portNumber);
+            ChannelServices.RegisterChannel(tcpChannel, false);
+
+            var connector = typeof (GameServer);
+
+            RemotingConfiguration.RegisterWellKnownServiceType(connector, "TicTacToe", WellKnownObjectMode.Singleton);
+
+            Console.WriteLine("Press ENTER to quit");
+            Console.ReadKey();
+        }
+    }
+
+    class GameServer : GameServerConnector
+    {
+        public GameState GameState { get; set; }
+        public bool NewStateSince(DateTime date)
+        {
+            return false;
         }
     }
 }
